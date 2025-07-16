7a859f134b540389802a579bb75974ea8b470baf
Tibor Kovacs <tiborkovacs2001@gmail.com>
2025-07-16 11:14:26 +0200

    add backend init

---
+++ src/backend/FilmForum/src/main/java/com/tcs/miniproject/FilmForum/FilmForumApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.miniproject.FilmForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmForumApplication.class, args);
+	}
+
+}
---
+++ src/backend/FilmForum/src/test/java/com/tcs/miniproject/FilmForum/FilmForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.miniproject.FilmForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
