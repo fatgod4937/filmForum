b86a5a74afd41468dcc01d56fee0ee5ad19699ca
fatgod4937 <dergeczakos1208@gmail.com>
2025-07-17 14:28:40 +0200

    [add] layout, page

---
+++ src/frontend/app/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+import "./globals.css";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <Navbar />
+            <body>{children}</body>
+        </html>
+    );
+}
