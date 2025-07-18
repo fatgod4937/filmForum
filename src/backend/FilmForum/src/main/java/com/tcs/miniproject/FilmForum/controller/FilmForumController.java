5a1245c468ca5dc637586dd59159ad7a5426ad4b
Tibor Kovacs <tiborkovacs2001@gmail.com>
2025-07-18 17:33:32 +0200

    add cors allow all origin

--- src/backend/FilmForum/src/main/java/com/tcs/miniproject/FilmForum/controller/FilmForumController.java
+++ src/backend/FilmForum/src/main/java/com/tcs/miniproject/FilmForum/controller/FilmForumController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/film_forum")
 public class FilmForumController {
     private FilmForumService filmForumService;
